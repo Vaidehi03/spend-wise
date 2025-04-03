6fb9188fd8a85dac02100c884e9dfbd2e469a8fb
Pranav Srivastava <pranavsrivastava453@gmail.com>
2025-04-03 22:16:58 +0530

    Delete boilerplate for fastAPI app

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.user import User
-from app.models.transaction import Transaction
-from app.models.category import Category
-from app.models.category_keyword import CategoryKeyword
-
-# Import other models as you create them
